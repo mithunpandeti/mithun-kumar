953101d7a293cfe67f85d476eeefdbac7234153a
mithunpandeti <mithunpandeti@gmail.com>
2016-09-20 16:26:19 +0530

    Add files via upload

--- build.java
+++ build.java
@@ -1,1 +1,23 @@
-
+public class build
+{
+public static void main(String[]args)
+{
+String s="abbCCCddBBBxx";
+StringBuilder sb = new StringBuilder(s);
+		int maxcount = 0, count=0;
+		maxcount++;
+		count++;
+		for(int i=1; i<sb.length(); i++)
+		{
+			
+			if(sb.charAt(i) == sb.charAt(i-1)) {
+				count++;
+				if(count > maxcount)
+					maxcount = count;
+			}
+			else if(sb.charAt(i) != sb.charAt(i-1))
+				count = 1;
+				}
+				System.out.println(maxcount);
+				}
+	}
