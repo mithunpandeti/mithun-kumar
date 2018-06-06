aae94c54392d887de73ab2d0cacf0a04926ab5b8
Pandeti Mithun Kumar <mithunpandeti@gmail.com>
2018-06-06 18:25:57 +0530

    Create spaces.c

---
+++ spaces.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+char name[50];
+clrscr();
+gets(name);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("number of spaces in your entered string is: %d",count);
+getch();
+}
